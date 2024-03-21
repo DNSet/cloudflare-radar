15c625d47d3f78646d9d2386a79b8a0c7e6d7155
Mingzhe Zou <zoumingzhe@qq.com>
2024-03-21 21:09:19 +0800

    utils: domain_ranking: add load

--- cloudflare_radar/utils/ranking.py
+++ cloudflare_radar/utils/ranking.py
@@ -1,5 +1,7 @@
 # coding:utf-8
 
+import csv
+import os
 from typing import Optional
 
 from pycountry.db import Country
@@ -8,11 +10,12 @@ from xarg import commands
 from .request import download
 
 
-class domain_ranking():
+class domain_ranking(list):
     PREFIX = "http://radar.cloudflare.com/charts/TopDomainsTable"
 
     def __init__(self, location: Optional[Country] = None):
         self.__location: Optional[Country] = location
+        super().__init__()
 
     @property
     def location(self) -> str:
@@ -23,9 +26,17 @@ class domain_ranking():
         location: str = self.__location.alpha_2 if self.__location else ""
         return f"{self.PREFIX}/attachment?location={location.lower()}"
 
+    def load(self, path: str) -> bool:
+        if not os.path.isfile(path):
+            return False
+        self.clear()
+        for line in sorted(csv.DictReader(open(path)),
+                           key=lambda x: int(x["rank"])):
+            self.append(line["domain"])
+        return True
+
     def download(self, path: str) -> bool:
         filepath: Optional[str] = download(url=self.url, path=path)
-        success: bool = True if isinstance(filepath, str) else False
-        status: str = "success" if success else "failed"
+        status: str = "success" if isinstance(filepath, str) else "failed"
         commands().logger.info(f"download {self.url} {status}")
-        return success
+        return self.load(filepath) if isinstance(filepath, str) else False
